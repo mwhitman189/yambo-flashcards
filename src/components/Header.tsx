b7e8ff8c1048a03157755ff68e5fff79b85daf6d
mwhitman189 <mwhitman189@gmail.com>
2022-06-19 14:57:52 +0000

    [Task] 2022/06/19 - Miles - Update README.md

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
