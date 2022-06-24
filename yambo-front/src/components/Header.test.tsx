07713ab0efdf84bd40a73ab307d77ea10eb38677
mwhitman189 <mwhitman189@gmail.com>
2022-06-24 13:28:13 +0000

    [Task] 2022/06/24 - Miles - Resolve #4; Move the .git folder into the main directory

--- yambo-front/src/components/Header.test.tsx
+++ yambo-front/src/components/Header.test.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import Header from './components/Header';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Header from './Header';
 
 
 test('displays the title "Yambo!"', () => {
