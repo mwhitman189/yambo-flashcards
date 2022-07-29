c7092f866e7846955375fae2a2147f647cc4e600
Will <mitchellspaur@gmail.com>
2022-07-29 20:28:00 +0900

    initial backend build commit

---
+++ yambo-back/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: __dirname + "/config/.env" });
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routers/user_router");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/user", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
